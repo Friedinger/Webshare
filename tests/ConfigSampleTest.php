3a77015f1ce94685b3be0835451e95bac3532e1a
Friedinger <71267611+Friedinger@users.noreply.github.com>
2023-11-05 01:23:07 +0100

    Unit Test Setup

---
+++ tests/ConfigSampleTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class ConfigSampleTest extends TestCase
+{
+	protected function setUp(): void
+	{
+		$_SERVER["DOCUMENT_ROOT"] = __DIR__ . "/../home/";
+		require_once($_SERVER["DOCUMENT_ROOT"] . "/../webshare/webshareConfig_sample.php");
+	}
+	public function testStrings()
+	{
+		$this->assertIsString(Webshare\Config::INSTALL_PATH);
+		$this->assertIsString(Webshare\Config::PATH_STORAGE);
+		$this->assertIsString(Webshare\Config::PATH_ADMIN);
+		$this->assertIsString(Webshare\Config::PATH_VIEW);
+		$this->assertIsString(Webshare\Config::PATH_PASSWORD);
+		$this->assertIsString(Webshare\Config::PATH_DELETE);
+		$this->assertIsString(Webshare\Config::DB_HOST);
+		$this->assertIsString(Webshare\Config::DB_USERNAME);
+		$this->assertIsString(Webshare\Config::DB_PASSWORD);
+		$this->assertIsString(Webshare\Config::DB_NAME);
+		$this->assertIsString(Webshare\Config::DB_TABLE);
+	}
+	public function testArrayTextForm()
+	{
+		$this->assertIsArray(Webshare\Config::TEXT_FORM);
+		$keys = [
+			"labelUri", "labelFile", "labelExpireDate", "labelPassword", "labelSeparator", "buttonAdd", "buttonDelete", "buttonPassword",
+		];
+		foreach ($keys as $key) {
+			$this->assertArrayHasKey($key, Webshare\Config::TEXT_FORM);
+		}
+	}
+	public function testArrayTextAdd()
+	{
+		$this->assertIsArray(Webshare\Config::TEXT_ADD);
+		$keys = [
+			"success", "errorUri", "errorBoth", "errorUploadSize", "error",
+		];
+		foreach ($keys as $key) {
+			$this->assertArrayHasKey($key, Webshare\Config::TEXT_ADD);
+		}
+	}
+	public function testArrayTextDelete()
+	{
+		$this->assertIsArray(Webshare\Config::TEXT_DELETE);
+		$keys = [
+			"heading", "default", "success", "error",
+		];
+		foreach ($keys as $key) {
+			$this->assertArrayHasKey($key, Webshare\Config::TEXT_DELETE);
+		}
+	}
+	public function testArrayTextPassword()
+	{
+		$this->assertIsArray(Webshare\Config::TEXT_PASSWORD);
+		$keys = [
+			"heading", "default", "incorrect",
+		];
+		foreach ($keys as $key) {
+			$this->assertArrayHasKey($key, Webshare\Config::TEXT_PASSWORD);
+		}
+	}
+	public function testFunctions()
+	{
+		$this->assertNull(Webshare\Config::error404());
+		$this->assertIsBool(Webshare\Config::adminAccess());
+		$this->assertNull(Webshare\Config::noAdminAccess());
+	}
+}
---
+++ tests/IndexTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class IndexTest extends TestCase
+{
+	protected function setUp(): void
+	{
+		$_SERVER["DOCUMENT_ROOT"] = __DIR__ . "/../home/";
+		$_SERVER["REQUEST_URI"] = "/";
+	}
+	public function testIndex()
+	{
+		require_once($_SERVER["DOCUMENT_ROOT"] . "index.php");
+		$this->assertIsObject($webshare);
+	}
+}
