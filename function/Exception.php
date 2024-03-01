23567a97206ab1e2f60bf0e45669fa1707887143
Friedinger <71267611+Friedinger@users.noreply.github.com>
2024-03-01 13:13:59 +0100

    Add exceptions

---
+++ function/Exception.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Webshare;
+
+use \Exception;
+
+class DatabaseException extends Exception
+{
+}
+
+class ShareException extends Exception
+{
+}
+
+class PermissionException extends Exception
+{
+}
